27b57b4c02b6ce46ea4b4e8bc4b83f10192a48ed
Hannah Lovelace <escardin@users.noreply.github.com>
2022-02-01 13:09:12 -0500

    Fix use of shell & sudo to write files

--- blink.py
+++ blink.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python 3
 
-from gpiozero import LED
 from flask import Flask, jsonify
 from time import sleep
 import threading
-import os
 
 app = Flask(__name__)
 
-def blink(led):
+ON = "1"
+OFF = "0"
+
+def ledSet(targetState):
+    with open('/sys/class/leds/led1/brightness','w') as f:
+        f.write(targetState)
+
+def blink():
     t = threading.currentThread()
     while getattr(t, "run_b", True):
         if getattr(t, "blink", False):
-            os.system('echo 1 | sudo dd status=none of=/sys/class/leds/led1/brightness') # led on
+            ledSet(ON)
             sleep(0.5)
-            os.system('echo 0 | sudo dd status=none of=/sys/class/leds/led1/brightness') # led off
+            ledSet(OFF)
             sleep(0.5)
 
 
-proc = threading.Thread(target=blink,args=(os,))
+proc = threading.Thread(target=blink,args=())
 proc.run_b = True
 proc.blink = False
 proc.start()
@@ -33,7 +38,7 @@ def ledOn():
 @app.route("/off", methods=['GET'])
 def ledOff():
     proc.blink = False
-    os.system('echo 0 | sudo dd status=none of=/sys/class/leds/led1/brightness') # led off
+    ledSet(OFF)
     return jsonify({'message': 'turning off'})
 
 
@@ -41,4 +46,4 @@ app.run(host='0.0.0.0')
 
 proc.run_b = False
 proc.join(3)
-os.system('echo 0 | sudo dd status=none of=/sys/class/leds/led1/brightness') # led off
+ledSet(OFF)
